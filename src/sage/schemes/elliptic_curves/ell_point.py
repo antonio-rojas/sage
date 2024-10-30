39e75a9f813ce901e01484f7f92500b2e7a70127
Antonio Rojas <arojas@archlinux.org>
2024-10-30 21:53:58 +0100

    Replace prec_pari_to_bits usage
    Co-authored-by: Gonzalo Tornaría <tornaria@cmat.edu.uy>

--- src/sage/schemes/elliptic_curves/ell_point.py
+++ src/sage/schemes/elliptic_curves/ell_point.py
@@ -159,7 +159,6 @@ lazy_import('sage.schemes.generic.morphism', 'SchemeMorphism')
 
 try:
     from sage.libs.pari.all import pari, PariError
-    from cypari2.pari_instance import prec_pari_to_bits
 except ImportError:
     PariError = ()
 
